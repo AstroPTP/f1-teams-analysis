40502a6af585d43385bcaea8ea0822af8f63ff70
Thanh Phu Phan <21664322@student.curtin.edu.au>
2024-06-28 10:55:57 +0800

    Loop to ask user enter number of team again if user put number of teams < 2

--- DataEntryProgram.java
+++ DataEntryProgram.java
@@ -7,10 +7,18 @@ public class DataEntryProgram {
         System.out.println("Welcome to the FIA F1 Data Entry Program");
         System.out.println("=====================");
 
-        int teamNum = 0;
-        System.out.println("How many teams are there you want to enter?");
-        System.out.print("> Enter: ");
-        teamNum = sc.nextInt();
+        int teamNum;
+
+        // Ask user to enter again if user entered number of teams < 2
+        do {
+            System.out.println("How many teams are there you want to enter?");
+            System.out.print("> Enter: ");
+            teamNum = sc.nextInt();
+
+            if (teamNum < 2) {
+                System.out.println("Number of teams have to be greater than 2.");
+            }
+        } while (teamNum < 2);
 
         sc.close(); // Close the scanner
     }
